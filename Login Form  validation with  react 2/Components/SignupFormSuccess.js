a2964c2b231a727f6432188d77bde9adf9280ab9
Neha Singh <neha24086@gmail.com>
2022-02-26 20:24:20 +0530

    login form2

---
+++ Login Form  validation with  react 2/Components/SignupFormSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignupForm from './SignupForm'
+
+const SignupFormSuccess = () => {
+  return (
+    <div className='container'>
+        <div className='app-wrapper'>
+            <h1 className='form-success'>Account is Created</h1>
+        </div>
+    </div>
+  )
+}
+
+export default SignupFormSuccess
