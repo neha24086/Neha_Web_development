d9e6f23caf6649efed01729ae0642f1a569c87d1
Neha Singh <neha24086@gmail.com>
2022-02-28 00:51:07 +0530

    sidenavbar-New

---
+++ Sidenavbar-New/pages/Order.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Order = () => {
+  return (
+    <div className='title'>Order</div>
+  )
+}
+
+export default Order
---
+++ Sidenavbar-New/pages/Saved.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Saved = () => {
+  return (
+    <div className='title'>Saved</div>
+  )
+}
+
+export default Saved
